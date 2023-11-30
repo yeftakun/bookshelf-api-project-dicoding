6de5e093fac03fb34aa39039931a329571233337
Yefta Asyel <yeftakun34@gmail.com>
2023-11-30 17:03:00 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const hapi = require('@hapi/hapi');
+const rute = require('./routes');
+
+const init = async () => {
+  const server = hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(rute);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
